10d6fc818f8915a7a559fc2f896c80af163b9042
Harshyadav02 <harshyadav5736@gmail.com>
2023-01-26 19:11:37 +0530

    Dromey

--- Chapter_4/4.3.py
+++ Chapter_4/4.3.py
@@ -1,8 +1,8 @@
 # find the Maximum Number in an set
 import math
 def MaxNum(list):
-    greatNum = -math.inf
-    for i in range(0,len(list)):
+    greatNum = list[0]
+    for i in range(1,len(list)):
         greatNum  = max(greatNum ,list[i])
     return greatNum
 list = [0,8,9,-1,11]
