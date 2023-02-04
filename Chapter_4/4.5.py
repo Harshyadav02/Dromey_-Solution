ecf0fe1cffea6a608bb36e5dfce606e8b8d10a74
Harshyadav02 <harshyadav5736@gmail.com>
2023-02-04 17:12:51 +0530

    How to solve it by Computer.

---
+++ Chapter_4/4.5.py
@@ -0,0 +1,22 @@
+'''Given a randomly ordered array of n element ,partition the element into two subset 
+such that elements <= x are in one subset and element > x are in the other subset
+
+Condition:- 1. Don't use sort method 
+            2. Ordered sub-array is not required 
+'''
+def partarr(arr,x):
+    i = 0
+    j = len(arr)-1
+    while(i < j):
+        if arr[i] > x:
+            if arr[j] <= x:
+                arr[i] ,arr[j] = arr[j] ,arr[i]
+                j -=1
+            else:
+                j -=1
+        else:
+            i += 1
+    return arr
+arr = [200,2,49,38,4,3,2,100,2]
+print(partarr(arr,4))
+
