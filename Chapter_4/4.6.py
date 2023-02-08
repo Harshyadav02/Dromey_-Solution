0ca98e9b5c5c220320333a5d96a76a9bab35916d
Harshyadav02 <harshyadav5736@gmail.com>
2023-02-08 12:48:27 +0530

    How to solve it by computer.

--- Chapter_4/4.6.py
+++ Chapter_4/4.6.py
@@ -1,14 +1,20 @@
+''' Find the  kth Smallest element form the given list '''
 def kth_Smallele(arr,k):
     arr.sort()
     count = 0
     i = 0
     while(count != k-1):
         if  count == len(arr)-1:
-            print("the last indexed element is the kth smallest element so,")
+            print(" No other element is present in the list therefore,",end="")
             break
-        if arr[i] < arr[i+1]:
-            count +=1
-            i +=1
+        elif k >0:
+            if arr[i] < arr[i+1]:
+                count +=1
+                i +=1
+        else:
+            print("Negative kth element cannot be found ")
+            return -1
+            
     return arr[i]
 arr = [1,100,200,2,3,5,8]
-print("The kth smallest element is: ",kth_Smallele(arr,100))
+print("The kth smallest element is: ",kth_Smallele(arr,8))
