d62bebb1fed6958785e95dc2b4765ecfc27b8164
Harshyadav02 <harshyadav5736@gmail.com>
2023-03-06 16:32:26 +0530

    How to solve it by computer

--- Chapter_5/Suppl_problem/5.4_Supply_Problem/5.4.4.py
+++ Chapter_5/Suppl_problem/5.4_Supply_Problem/5.4.4.py
@@ -6,20 +6,20 @@ class sort:
             key = self.arr[i]
             left = 0
             right = i - 1
-            
+            # Finding the element to insert it into its Right Postion
             while left <= right:
                 mid = (left + right) // 2
                 if self.arr[mid] < key:
                     left = mid + 1
                 else:
                     right = mid - 1
-                    
+            # Shifting the element to make the space         
             for j in range(i - 1, left - 1, -1):
                 self.arr[j + 1] = self.arr[j]
-                
+            # Placing the Element into its right Position    
             self.arr[left] = key
 
         return self.arr
-arr = [6,1,0,3,2]
+arr = [5,4,3,2,1]
 s = sort(arr)
 print(s.insertion_sort_binary())
